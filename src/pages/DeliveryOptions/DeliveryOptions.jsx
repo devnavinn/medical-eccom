af7648516d18a3e72eb7ad29d071eab43c6425da
neo062 <navinkumar241999@gmail.com>
2024-02-18 14:59:32 +0530

    delivery option form

--- src/pages/DeliveryOptions/DeliveryOptions.jsx
+++ src/pages/DeliveryOptions/DeliveryOptions.jsx
@@ -1,5 +1,6 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { orderPlace } from "../../api/api"
@@ -25,11 +26,18 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
 const FormSchema = z.object({
     reciepient: z.string().nonempty({ message: "Recipient is required." }),
+    companyName: z.string().optional(),
+    street: z.string().optional(),
+    zip: z.string().optional(),
+    city: z.string().optional(),
     switchToCurabox: z.boolean(),
     deliveryStart: z.string().nonempty({ message: "Delivery start is required." }),
 })
-
+import { useTranslation } from "react-i18next"
 export default function DeliveryOptions() {
+    const { t } = useTranslation()
+    const { deliveryOptions } = t("specify-data")
+    const { heading1, select, companyName, street, postCode, city, heading2, isAgree, deliveryStart, button } = deliveryOptions
     const navigate = useNavigate()
     const form = useForm({
         resolver: zodResolver(FormSchema),
@@ -53,28 +61,33 @@ export default function DeliveryOptions() {
     const onErrors = (errors) => {
         console.log(errors)
     }
+    const [isCommissionedServiceSelected, setIsCommissionedServiceSelected] = useState(false);
 
 
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit, onErrors)} className="w-full space-y-6">
-                <h3 className="text-2xl text-[#003780] mt-5">Where should the curabox be delivered to?</h3>
+                <h3 className="text-2xl text-[#003780] mt-5">{heading1}</h3>
                 <FormField
                     control={form.control}
                     name="reciepient"
                     render={({ field }) => (
                         <FormItem>
-                            <FormLabel>Select recipient</FormLabel>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                            <FormLabel>{select?.label}</FormLabel>
+                            <Select onValueChange={(value) => {
+                                field.onChange(value);
+                                setIsCommissionedServiceSelected(value === "To a commissioned nursing service" || value === "An einen beauftragten Pflegedienst");
+                            }}
+                                defaultValue={field.value}>
                                 <FormControl>
                                     <SelectTrigger>
-                                        <SelectValue placeholder="Select recipient" />
+                                        <SelectValue placeholder={select?.label} />
                                     </SelectTrigger>
                                 </FormControl>
                                 <SelectContent>
-                                    <SelectItem value="To the insured person">To the insured person</SelectItem>
-                                    <SelectItem value="To the Caregiver">To the Caregiver</SelectItem>
-                                    <SelectItem value="To a commissioned nursing service">To a commissioned nursing service</SelectItem>
+                                    <SelectItem value={select?.value1}>{select?.value1}</SelectItem>
+                                    <SelectItem value={select?.value2}>{select?.value2}</SelectItem>
+                                    <SelectItem value={select?.value3}>{select?.value3}</SelectItem>
                                 </SelectContent>
                             </Select>
 
@@ -82,8 +95,62 @@ export default function DeliveryOptions() {
                         </FormItem>
                     )}
                 />
+                <div className={`${isCommissionedServiceSelected ? 'block' : 'hidden'} grid grid-cols-1 md:grid-cols-2 gap-5`}>
+                    <FormField
+                        control={form.control}
+                        name="companyName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{companyName?.label}</FormLabel>
+                                <FormControl>
+                                    <Input type='text' placeholder={companyName?.placeholder}  {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="street"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{street?.label}</FormLabel>
+                                <FormControl>
+                                    <Input type='text' placeholder={street?.placeholder}  {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="zip"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{postCode?.label}</FormLabel>
+                                <FormControl>
+                                    <Input type='text' placeholder={postCode?.placeholder}  {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="city"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{city?.label}</FormLabel>
+                                <FormControl>
+                                    <Input type='text' placeholder={city?.placeholder}  {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
                 <div className="mt-10">
-                    <h3 className="text-2xl text-[#003780] mb-5">Information about changing supplier</h3>
+                    <h3 className="text-2xl text-[#003780] mb-5">{heading2}</h3>
 
                     <FormField
                         control={form.control}
@@ -98,7 +165,7 @@ export default function DeliveryOptions() {
                                 </FormControl>
                                 <div className="space-y-1 leading-none">
                                     <FormLabel>
-                                        I already receive care aids from another provider, but would like to be supplied by curabox in the future.
+                                        {isAgree}
                                     </FormLabel>
 
                                 </div>
@@ -111,7 +178,7 @@ export default function DeliveryOptions() {
                             name="deliveryStart"
                             render={({ field }) => (
                                 <FormItem>
-                                    <FormLabel>Desired start of delivery *</FormLabel>
+                                    <FormLabel>{deliveryStart?.label}*</FormLabel>
                                     <FormControl>
                                         <Input type='date'  {...field} />
                                     </FormControl>
@@ -121,7 +188,7 @@ export default function DeliveryOptions() {
                         />
                     </div>
                 </div>
-                <Button type="submit" className='py-2 px-12 md:px-24 rounded-3xl bg-[#003780] text-white'>Continue to overview and signature</Button>
+                <Button type="submit" className='py-2 px-12 md:px-24 rounded-3xl bg-[#003780] text-white'>{button}</Button>
             </form>
         </Form>
     )
